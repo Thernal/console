cc3d31dc14a8460dce233c090c0927ef34b43b36
hhumbetovv <hhumbetovv@gmail.com>
2026-06-13 15:23:48 +0400

    fix: stabilize intermittent log detail and collapsible scroll

--- addons/logging-ui/src/commonMain/kotlin/io/thernal/console/logging/ui/view/detail/components/LogDetailPager.kt
+++ addons/logging-ui/src/commonMain/kotlin/io/thernal/console/logging/ui/view/detail/components/LogDetailPager.kt
@@ -1,13 +1,14 @@
 package io.thernal.console.logging.ui.view.detail.components
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import io.thernal.console.api.ui.LogRenderer
 import io.thernal.console.designsystem.foundation.theme.Theme
@@ -23,12 +24,15 @@ fun LogDetailPager(
         state = pagerState,
         modifier = Modifier.fillMaxSize(),
         pageSpacing = Theme.dimens.dp12,
+        userScrollEnabled = logs.value.size > 1,
     ) { pageIndex ->
         logs.value.getOrNull(pageIndex)?.let { pageLog ->
+            val scrollState = remember(pageLog.id) { ScrollState(initial = 0) }
+
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .verticalScroll(rememberScrollState()),
+                    .verticalScroll(scrollState),
             ) {
                 renderer.Detail(log = pageLog)
             }
